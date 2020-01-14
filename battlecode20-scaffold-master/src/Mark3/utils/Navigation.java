91dca9621d0490d3d29f799897d4b124c970b9f3
milenicnikola <milenicnikola@gmail.com>
2020-01-14 18:46:05 +0100

    Dodata dokumentacija

--- battlecode20-scaffold-master/src/Mark3/utils/Navigation.java
+++ battlecode20-scaffold-master/src/Mark3/utils/Navigation.java
@@ -39,8 +39,16 @@ public class Navigation {
         return Math.abs(distance.x) == Math.abs(distance.y);
     }
 
+    /**
+     * the amount of obstacles encountered while bug pathing
+     */
     public static int frustration = 0;
 
+    /**
+     * Checks if a location is suitable for offensive landing
+     * @param location The location to check. It needs to be within the map boundaries.
+     * @return true if it is known to be a good landing spot, false otherwise.
+     */
     public static boolean goodLandingSpot(MapLocation location){
         if(location == null) return false;
         if(Strategium.enemyHQLocation == null) return false;
@@ -48,8 +56,7 @@ public class Navigation {
         if(location.distanceSquaredTo(Strategium.enemyHQLocation) > RobotType.LANDSCAPER.sensorRadiusSquared)
             return false;
         if(location.isAdjacentTo(Strategium.enemyHQLocation)) return true;
-        if(Strategium.elevation[location.x][location.y] > 15) return true;
-        return false;
+        return Strategium.elevation[location.x][location.y] > 15;
     }
 
     private static boolean isOnLine(MapLocation destination) {
@@ -62,10 +69,21 @@ public class Navigation {
         return Math.abs(point.x) == Math.abs(point.y);
     }
 
+    /**
+     * Moves the unit one step towards the destination, circumnavigating any hazards and obstacles along the way.
+     * The field "frustration" represents the amount of obstacles encountered along the way. It can and should be used
+     * to determine if the destination is unreachable, the unit got stuck or it has spent a certain amount of time
+     * circling the target.
+     * @param destination the location to go towards
+     * @return true if move was made, false otherwise
+     * @throws GameActionException it doesn't
+     */
     public static boolean bugPath(MapLocation destination) throws GameActionException {
         Direction dir = moveTowards(destination);
-        if(dir != moveTowards(lastDestination))
+        if(dir != moveTowards(lastDestination)) {
+            frustration = 0;
             avoiding = false;
+        }
 
         lastDestination = destination;
 
@@ -107,7 +125,7 @@ public class Navigation {
         frustration++;
         dir = lastAvoidingDirection;
 
-        int i = 0;
+        int i;
         for(i = 0; Strategium.canSafelyMove(dir); dir = dir.rotateRight(), i++) {
             if (i == 8) {
                 avoiding = false;
@@ -308,9 +326,13 @@ public class Navigation {
         ret.add(Direction.WEST);
         ret.add(Direction.NORTH);
         return ret;
-
     }
 
+    /**
+     * Calculates a direction in which the unit should move in order to follow a clockwise square path.
+     * @param center center of the square
+     * @return direction to move in
+     */
     public static Direction clockwiseSquare(MapLocation center) {
 
         if (center == null) return Direction.CENTER;
@@ -328,29 +350,68 @@ public class Navigation {
         return Direction.CENTER;
     }
 
+    /**
+     * Computes the aerial distance (ie. the distance between them along either x or y axis, whichever is larger)
+     * between the two points.
+     * @param source one point
+     * @param destination other point
+     * @return distance between the points
+     */
     public static int aerialDistance(MapLocation source, MapLocation destination) {
         if (source == null || destination == null) return Integer.MAX_VALUE;
         return Math.max(Math.abs(source.x - destination.x), Math.abs(source.y - destination.y));
     }
 
+    /**
+     * Computes the aerial distance (ie. the distance between them along either x or y axis, whichever is larger)
+     * between the two points.
+     * @param source one point
+     * @param destX other point x coordinate
+     * @param destY other point y coordinate
+     * @return distance between the points
+     */
     public static int aerialDistance(MapLocation source, int destX, int destY) {
         if (source == null) return Integer.MAX_VALUE;
         return Math.max(Math.abs(source.x - destX), Math.abs(source.y - destY));
     }
 
+    /**
+     * Computes the aerial distance (ie. the distance between them along either x or y axis, whichever is larger)
+     * to the target robot
+     * @param target target robot
+     * @return distance to the target robot
+     */
     public static int aerialDistance(RobotInfo target) {
         if (target == null) return Integer.MAX_VALUE;
         return aerialDistance(rc.getLocation(), target.location);
     }
 
+    /**
+     * Computes the aerial distance (ie. the distance between them along either x or y axis, whichever is larger)
+     * to the location
+     * @param destination point to compute the distance to
+     * @return distance to the target location
+     */
     public static int aerialDistance(MapLocation destination) {
         return aerialDistance(rc.getLocation(), destination);
     }
 
+    /**
+     * Computes the aerial distance (ie. the distance between them along either x or y axis, whichever is larger)
+     * to the location
+     * @param destX x coordinate of the point to compute the distance to
+     * @param destY y coordinate of the point to compute the distance to
+     * @return distance to the target location
+     */
     public static int aerialDistance(int destX, int destY) {
         return aerialDistance(rc.getLocation(), destX, destY);
     }
 
+    /**
+     * Clamps the location coordinates to be within the map boundaries
+     * @param location the location to clamp
+     * @return the location with clamped coordinates
+     */
     public static MapLocation clamp(MapLocation location){
         int x = location.x;
         int y = location.y;
