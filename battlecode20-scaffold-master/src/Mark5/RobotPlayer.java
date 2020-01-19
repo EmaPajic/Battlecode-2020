c1abd47554510f1ece0618d6afa6f67605202ebe
doraaki <nikolaaleksic44@gmail.com>
2020-01-19 19:32:06 +0100

    Randomized mining. Rush on.

--- battlecode20-scaffold-master/src/Mark5/RobotPlayer.java
+++ battlecode20-scaffold-master/src/Mark5/RobotPlayer.java
@@ -67,7 +67,7 @@ public strictfp class RobotPlayer {
         RobotPlayer.rc = rc;
         if (rc.getType() == RobotType.MINER)
             if (rc.getRoundNum() == 2) {
-                //myFun = 4;
+                myFun = 4;
             }
         Strategium.init();
 
